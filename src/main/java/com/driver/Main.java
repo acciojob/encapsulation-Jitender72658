ff4b106ce3880574c35cdd054650f3273fcd0a94
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-02-08 16:37:08 +0530

    submitting assignment solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+   void main(){
+     RWOnly obj = new RWOnly();
+    // System.out.println(obj.name);
+
+     // error: name has private access in RWOnly
+     obj.setter("Rohit");
+     System.out.println(obj.getter());
+    }
 }
