d5c527ccab50f2cd709c21b4e5d395bcd9c72da8
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-02-08 16:39:56 +0530

    setter and getter function name corrected

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,7 +6,7 @@ public class Main {
     // System.out.println(obj.name);
 
      // error: name has private access in RWOnly
-     obj.setter("Rohit");
-     System.out.println(obj.getter());
+     obj.setName("Rohit");
+     System.out.println(obj.getName());
     }
 }
